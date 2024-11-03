8df7a81dd0fe25e86d443138e8e42f29545e5cb7
MohammadGholamzade <mohammad.gholamzade74@gmail.com>
2024-11-03 21:30:44 +0330

    Hello

---
+++ src/Actio.Services.Activities/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actio.Services.Activities.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get() => Content("Hello From Actio.Services.Activities.Controllers API !");
+    }
+}
